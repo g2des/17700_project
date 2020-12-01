39777853e03995a8f7285c115edc12855da165b0
Gaurav <gauravdeshpande@cmu.edu>
2020-12-01 17:35:14 -0500

    Modifying behavior of makefile to add a custom URL

--- clipper/scripts/verify_clipper.py
+++ clipper/scripts/verify_clipper.py
@@ -1,19 +1,24 @@
-#!/usr/bin/env python\
-import requests, json, sys, numpy as np
-from clipper_admin import ClipperConnection, DockerContainerManager
+#!/usr/bin/env python
+import requests, json, sys, re, numpy as np
+# from clipper_admin import ClipperConnection, DockerContainerManager
 
+regex = re.compile('(?<=\[)(.*?)(?=\])')
 try:
     app_name = str(sys.argv[1])
-    print("Evaluating with ",  app_name,)
+    url = str(sys.argv[2])
+    print("Evaluating with ",  app_name, url)
 except Exception as e:
     print("ERROR!!"+str(e))
-    print("USAGE:\n\tpython build_deploy.py <app_name>")
+    print("USAGE:\n\tpython build_deploy.py <app_name> <url>")
 
 
-clipper_conn = ClipperConnection(DockerContainerManager())
-clipper_conn.connect()
-query_address =clipper_conn.get_query_addr()
+# clipper_conn = ClipperConnection(DockerContainerManager())
+# clipper_conn.connect()
+# query_address =clipper_conn.get_query_addr()
 
 headers = {"Content-type": "application/json"}
-print(requests.post(f"http://{query_address}/{app_name}/predict", headers=headers, data=json.dumps({
+response = (requests.post(f"http://{url}/{app_name}/predict", headers=headers, data=json.dumps({
     "input": "[Hello world!]"})).json())
+## Expected Result : {'query_id': 2, 'output': "b '[Bonjour monde !]'", 'default': False}
+print(regex.findall(response['output']))
+
