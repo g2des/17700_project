39c1de3e406ef5bb3a4b305226a30cff785baeb8
Gaurav <gauravdeshpande@cmu.edu>
2020-11-30 23:55:42 -0500

    Adding file for model verification.

--- clipper/scripts/verify_model.py
+++ clipper/scripts/verify_model.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python\
+try:
+    model_path = str(sys.argv[1])
+    print("Evaluating with ",  model_path,)
+except Exception as e:
+    print("ERROR!!"+str(e))
+    print("USAGE:\n\tpython build_deploy.py <model_path>")
+
 print("Importing model...")
-model = cloudpickle.load(open('./models.pkl','rb'))
+model = cloudpickle.load(open( model_path,'rb'))
 print(model.translate(['Hello world!','This is clipper world','Wait whaat!'], beam=5))
